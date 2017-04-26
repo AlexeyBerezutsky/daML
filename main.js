4962693e8ad6d8be2220da6fcd745b5a98d89bd9
Alexey Berezutsky <AlexeyBerezutsky@gmail.com>
2017-04-26 22:55:20 +0300

    added modal

--- main.js
+++ main.js
@@ -63,6 +63,8 @@ var mapController = (function ($) {
 
         var url = createUrl('http://localhost:8080');
 
+        $('.modal-inprogress')[0].active = true;
+
         var jqxhr = fakeGet(url, function (data) {
             removeMarkers(markerContainer);
 
@@ -77,6 +79,8 @@ var mapController = (function ($) {
             drawMarkers(markerContainer, map);
 
             setSearchRadius();
+
+            $('.modal-inprogress')[0].active = false;
         })
     };
 
@@ -242,8 +246,10 @@ var initMap = function () {
 };
 
 var fakeGet = function (url, response) {
-
+window.setTimeout(function () {
     console.log(url);
     var json = '{ "pinlat": "59.957570", "pinlng": "30.307946", "pinrad": "4000", "segments": [ { "lat": 59.9397513701937, "lng": 30.33033963504616, "rad": 417.3191710256402, "icon": "http://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Marker-Outside-Azure-icon.png", "places": [ { "lat": 59.94348796819766, "lng": 30.330861279429218, "rad": 0.0, "icon": "resources/icons/vista.ball.light.red.32.png", "id": "4c247fecf1272d7f5e1983c5", "name": "Field of Mars (Марсово поле)", "placeType": "PLAZA", "phone": null, "address": "Марсово поле", "city": "Saint_Petersburg", "country": "Russia", "source": "Foursquare", "additionalInfo": "checkinsCoun=56218", "url": null, "rating": 127524.0, "point": [ 59.94348796819766, 30.330861279429218 ], "latLng": { "lat": 59.94348796819766, "lng": 30.330861279429218 } }, { "lat": 59.940027808329596, "lng": 30.332906648768653, "rad": 0.0, "icon": "resources/icons/vista.ball.green.32.png", "id": "4c162f2d82a3c9b666dbfff8", "name": "Mikhailovsky Garden (Михайловский сад)", "placeType": "PARK", "phone": null, "address": "Садовая ул.", "city": "Saint_Petersburg", "country": "Russia", "source": "Foursquare", "additionalInfo": "checkinsCoun=23401", "url": null, "rating": 35248.0, "point": [ 59.940027808329596, 30.332906648768653 ], "latLng": { "lat": 59.940027808329596, "lng": 30.332906648768653 } }, { "lat": 59.93871704466662, "lng": 30.332318544387817, "rad": 0.0, "icon": "resources/icons/vista.ball.bronze.32.png", "id": "4bbc64cd51b89c742155872a", "name": "The State Russian Museum (Русский музей)", "placeType": "MUSEUM", "phone": "+7 812 595-42-48", "address": "Инженерная ул., 4", "city": "Saint_Petersburg", "country": "Russia", "source": "Foursquare", "additionalInfo": "checkinsCoun=23105", "url": "http://www.rusmuseum.ru", "rating": 30909.0, "point": [ 59.93871704466662, 30.332318544387817 ], "latLng": { "lat": 59.93871704466662, "lng": 30.332318544387817 } }, { "lat": 59.93866111775865, "lng": 30.328853130340576, "rad": 0.0, "icon": "resources/icons/vista.ball.bronze.32.png", "id": "4dbd3e31fa8cee727361cfae", "name": "The Russian Museum (Benois Wing) (Русский музей (Корпус Бенуа))", "placeType": "MUSEUM", "phone": "+7 812 595-42-48", "address": "наб. Канала Грибоедова, 2", "city": "Saint_Petersburg", "country": "Russia", "source": "Foursquare", "additionalInfo": "checkinsCoun=13319", "url": "http://www.rusmuseum.ru/benois-wing/", "rating": 17736.0, "point": [ 59.93866111775865, 30.328853130340576 ], "latLng": { "lat": 59.93866111775865, "lng": 30.328853130340576 } }, { "lat": 59.94081730579484, "lng": 30.325173583774674, "rad": 0.0, "icon": "resources/icons/vista.ball.light.red.32.png", "id": "4db982a24df0ded98bd3742c", "name": "Конюшенная площадь", "placeType": "PLAZA", "phone": null, "address": "Конюшенная ул.", "city": "Saint_Petersburg", "country": "Russia", "source": "Foursquare", "additionalInfo": "checkinsCoun=6978", "url": null, "rating": 14759.0, "point": [ 59.94081730579484, 30.325173583774674 ], "latLng": { "lat": 59.94081730579484, "lng": 30.325173583774674 } }, { "lat": 59.936796976414804, "lng": 30.33192462357603, "rad": 0.0, "icon": "resources/icons/vista.ball.green.32.png", "id": "4c6ffeda344437045498225f", "name": "Arts Square (Площадь Искусств)", "placeType": "PARK", "phone": null, "address": "Инженерная ул.", "city": "Saint_Petersburg", "country": "Russia", "source": "Foursquare", "additionalInfo": "checkinsCoun=20600", "url": null, "rating": 42379.0, "point": [ 59.936796976414804, 30.33192462357603 ], "latLng": { "lat": 59.936796976414804, "lng": 30.33192462357603 } } ], "message": "Cluster rating=1229.0407546913643, radius=417.3191710256402", "rating": 1229.0407546913643, "point": [ 59.9397513701937, 30.33033963504616 ], "latLng": { "lat": 59.9397513701937, "lng": 30.33033963504616 } } ], "places": null }';
     response(JSON.parse(json));
+},3000);
+
 };
